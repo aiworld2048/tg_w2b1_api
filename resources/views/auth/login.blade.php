57fd5f7528b9c92ef917304389d405bae2b3122f
AMK1215 <aungmyokhaing1215@gmail.com>
2025-11-09 14:39:23 +0630

    user guide

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PoneWine | Login</title>
+    <title>TG Slot | Login</title>
 
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
@@ -306,7 +306,7 @@
     
     <div class="login-container">
         <div class="login-logo">
-            <h2>PoneWine</h2>
+            <h2>TG Slot</h2>
             <p class="login-subtitle">Welcome Back</p>
         </div>
         
